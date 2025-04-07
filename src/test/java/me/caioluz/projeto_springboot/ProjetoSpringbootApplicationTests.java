8b212ee264f5dc9dfdcf48e3fc774a5c61fb8030
caioluz <caiogustavo.luz@gmail.com>
2025-04-07 15:45:31 -0300

    Initial commit

---
+++ src/test/java/me/caioluz/projeto_springboot/ProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.caioluz.projeto_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
